387ea9fc870ef6e198cb402f0a6c9c81b7b123e3
ashlynnpai <ashlynnpai@gmail.com>
2014-10-19 16:58:50 +0000

    second commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,16 @@
 class PostsController < ApplicationController
-  def index
 
+  # GET /posts
+  # GET /posts.json
+  def index
+    @posts = Post.all
+  end
+
+  # GET /posts/1
+  # GET /posts/1.json
+  def show
+    @post = Post.find(params[:id])
   end
 end
+
+
