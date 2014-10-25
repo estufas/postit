80b636c8ce2dfb7eb8624b2419d3e2cd35ca453f
ashlynnpai <ashlynnpai@gmail.com>
2014-10-25 23:04:59 +0000

    third commit

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,36 @@
+class CategoriesController < ActionController::Base
+  
+  def new
+    @category = Category.new
+  end
+  
+  def create
+    @category = Category.new(category_params)
+    
+    if @category.save
+      flash[:notice] = "Category has been created."
+      redirect_to posts_path     
+    else
+      render :new
+    end
+  end
+  
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+  
+  def update
+    @category = Category.find(params[:id])
+  end
+  
+  private
+  
+  def category_params
+    params.require(:category).permit(:name)
+  end 
+  
+end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,55 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
 
-  # GET /posts
-  # GET /posts.json
   def index
     @posts = Post.all
   end
 
-  # GET /posts/1
-  # GET /posts/1.json
   def show
+    @comment = Comment.new
+  end
+  
+  def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new(post_params)
+    @post.creator = User.first
+    
+    if @post.save
+      flash[:notice] = "Your post was created."
+      redirect_to posts_path     
+    else
+      render :new
+    end
+  end
+  
+  def edit
     @post = Post.find(params[:id])
   end
+  
+  def update
+    @post = Post.find(params[:id])
+    
+    if @post.update(post_params)
+      flash[:notice] = "The post was updated"
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+    
+  end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :url, :description)
+  end
+  
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
 end
-
-
